d13c6645f62da9a7760032ec40fac2494ac9dbac
Jorge Moscat <jorgemoscat@gmail.com>
2012-11-05 00:18:48 +0100

    Gem file

---
+++ thanxup/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thanxup::Application.config.secret_token = '593ed806f6914bbb943f7df7cd0adc35def8299f0de85b025e8f4bc534c02640a1bc1f5633808dfcebf2412dca37716804c7463b560a4c81e35426317a919786'
